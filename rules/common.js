1bdf8fc4bbe71a16709f5cd785e3bcc4771f3b9a
builat <rf00rm@gmail.com>
2019-12-12 18:26:22 +0700

    fix file extention issue (#1)
    For more detail see: https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/imports.js#L139

--- rules/common.js
+++ rules/common.js
@@ -3,6 +3,16 @@ module.exports = {
   "max-len": ["error", { code: 100 }],
   "max-classes-per-file": [0],
   "import/no-extraneous-dependencies": [0],
+  "import/extensions": [
+    "error",
+    "ignorePackages",
+    {
+      js: "never",
+      jsx: "never",
+      ts: "never",
+      tsx: "never",
+    },
+  ],
   "prettier/prettier": "error",
   quotes: ["error", "double"],
   semi: ["error", "never"],
